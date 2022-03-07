764d1e5b9dbe8e1575e51be564f861d3342def3d
Ketlin Mello <58960468+ketlindemello@users.noreply.github.com>
2022-03-07 14:14:34 -0500

    Create sqs_message_data.py
    Data to create messages in sqs.

---
+++ sqs_message_data.py
@@ -0,0 +1,8 @@
+message_data = [{'ResponseMetadata': { 'RequestId': 'b796835c-5d03-58c1-a258-d530d5e9efe3'},
+            'MD5OfMessageBody': 'edf19b076ee08e28510eebd4b9ffe075',
+            'MD5OfMessageAttributes': '2greucd7f9869597d1907b4a0e01e0b',
+            'MessageId': '12359d-b553-43ff-b12b-36e7e2cad314'},
+           {'ResponseMetadata': { 'RequestId': 'wrr-5d03-58c1-a258-d530d5e9efe3'},
+           'MD5OfMessageBody': 'edf19b076ee08e28510eebd4b9ffe075',
+            'MD5OfMessageAttributes': '2efbcecd7f9869597d1907b4a0e01e0b',
+            'MessageId': '240007a3-b553-43ff-b1-56941-e7e2cad314'}]
